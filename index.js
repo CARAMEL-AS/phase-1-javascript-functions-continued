d3ef0126ecc440842454680dab1fca92bee82836
Aftab Sidhu <Aftab180@gmail.com>
2022-04-26 14:06:21 -0400

    commit tree 3497d415afb556311c05dc45570a4e9ad66d633e

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(string) {
+    return string ? 'This Saturday, I want to bathe my dog!' : 'This Saturday, I want to roller-skate!'
+}
+saturdayFun()
+
+function mondayWork(string1) {
+    if(string1){
+        return 'This Monday, I will work from home.'
+    } else {
+        return 'This Monday, I will go to the office.'
+    }
+}
+mondayWork()
+
+function wrapAdjective(string2 = '*') {
+    const call = 'You are'
+    return `${call} ${string2}${call1}${string2}!`
+}
+wrapAdjective()
